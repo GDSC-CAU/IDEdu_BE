1947d19796ba12cb1bb47e6d66284105893d20e9
JaehwanH <jec0101@naver.com>
2025-02-23 18:47:17 +0900

    add: ExceptionController 추가

---
+++ src/main/java/com/gdg/backend/common/exception/controller/ExceptionController.java
@@ -0,0 +1,37 @@
+package com.gdg.backend.common.exception.controller;
+
+
+import com.gdg.backend.common.response.ApiResponse;
+import com.gdg.backend.common.response.status.ErrorCode;
+import io.swagger.v3.oas.annotations.Hidden;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
+
+@Controller
+@RequiredArgsConstructor
+@Hidden // Swagger에 명시 X
+public class ExceptionController implements ErrorController {
+    private final ErrorAttributes errorAttributes;
+
+    @RequestMapping("/error")
+    @ResponseBody
+    public ApiResponse<Object> handlerError(WebRequest request) {
+        Map<String, Object> errorAttributes = this.errorAttributes.getErrorAttributes(request, ErrorAttributeOptions.defaults());
+
+        int status = (int) errorAttributes.getOrDefault("status", 500);
+        String message = (String) errorAttributes.getOrDefault("message", "Unexpected error");
+
+        System.out.println("CustomErrorController: received status : " + status);
+        System.out.println("CustomErrorController: error message - " + message);
+
+        return ApiResponse.onFailure(ErrorCode._INTERNAL_SERVER_ERROR.getCode(), "알 수 없는 에러입니다.", "ERROR");
+    }
+}
