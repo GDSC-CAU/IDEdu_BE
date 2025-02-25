3f57ef233534db15b17ca4e3e877c39be55dd691
JaehwanH <jec0101@naver.com>
2025-02-26 03:11:27 +0900

    feat: OT 구현 - 완전 느릴듯

--- src/main/java/com/gdg/backend/domain/document/entity/Document.java
+++ src/main/java/com/gdg/backend/domain/document/entity/Document.java
@@ -1,14 +1,21 @@
 package com.gdg.backend.domain.document.entity;
 
+import com.gdg.backend.common.entity.BaseTimeEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-public class Document {
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Document extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
