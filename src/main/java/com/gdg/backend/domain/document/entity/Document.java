05fde433462b3fea2c805bde102319f9a4535d8b
JaehwanH <jec0101@naver.com>
2025-03-11 08:43:59 +0900

    add: Document 아래 contentBuilder 필드 추가

--- src/main/java/com/gdg/backend/domain/document/entity/Document.java
+++ src/main/java/com/gdg/backend/domain/document/entity/Document.java
@@ -1,10 +1,7 @@
 package com.gdg.backend.domain.document.entity;
 
 import com.gdg.backend.common.entity.BaseTimeEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -23,4 +20,25 @@ public class Document extends BaseTimeEntity {
 
     @Setter
     private Long version;
+
+    /**
+     * content 수정용 StringBuilder (content 직접 수정은 String이므로 오래 걸림)
+     * !! 주의: DB 저장 전에 syncContentBuilder() 등으로 contentBuilder -> content 동기화 필요 !!
+     * */
+    @Transient
+    private StringBuilder contentBuilder;
+
+    @PostLoad // JPA가 DB에서 로드한 후 호출
+    public void initContentBuilder() {
+        this.contentBuilder = new StringBuilder(content != null ? content : "");
+    }
+
+    public StringBuilder getContentBuilder() {
+        if(contentBuilder == null) initContentBuilder();
+        return contentBuilder;
+    }
+
+    public void syncContentBuilder() {
+        if(contentBuilder != null) content = contentBuilder.toString();
+    }
 }
