738a4cdcf39b68ba5dbe0294826b273a34966e24
JaehwanH <54016683+ja7811@users.noreply.github.com>
2025-03-14 00:16:48 +0900

    [FEAT] 학생 도움 요청 기능 (#43)
    * feat: 도움 요청 기능
    
    * typo: log 출력 오타 수정
    
    * remove: StackTraceLimiter 삭제
    
    logback에 내장된 기능이므로 삭제
    
    * update: 도움 요청 수신 시 ACK

---
+++ src/main/java/com/gdg/backend/domain/helprequest/controller/HelpRequestController.java
@@ -0,0 +1,29 @@
+package com.gdg.backend.domain.helprequest.controller;
+
+import com.gdg.backend.domain.helprequest.dto.HelpRequestDto;
+import com.gdg.backend.domain.helprequest.service.HelpRequestService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class HelpRequestController {
+    private final SimpMessagingTemplate template;
+    private final HelpRequestService helpRequestService;
+
+    @MessageMapping("/help/{classroomId}")
+    public void handleHelpRequest(
+            @Valid HelpRequestDto request,
+            @DestinationVariable("classroomId") Long classroomId
+    ) {
+        log.info("help request from student id {} : (classroomId={})", request.getUserId(), classroomId);
+        helpRequestService.handleHelpRequest(classroomId, request);
+        template.convertAndSend("/sub/ack", "ACK");
+    }
+}
--- src/main/java/com/gdg/backend/domain/mapping/repository/IdeMemberRepository.java
+++ src/main/java/com/gdg/backend/domain/mapping/repository/IdeMemberRepository.java
@@ -4,6 +4,7 @@ import com.gdg.backend.domain.document.entity.Document;
 import com.gdg.backend.domain.mapping.IdeMember;
 import com.gdg.backend.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 import java.util.Optional;
@@ -11,4 +12,7 @@ import java.util.Optional;
 public interface IdeMemberRepository extends JpaRepository<IdeMember, Long> {
     Optional<IdeMember> findByMember(Member member);
     Optional<IdeMember> findByDocument(Document document);
+
+    @Query("SELECT i FROM IdeMember i JOIN FETCH i.document WHERE i.classroom.id = :classroomId and i.member.id = :memberId")
+    Optional<IdeMember> findByClassroomIdAndMemberIdFetchJoinDocument(Long classroomId, Long memberId);
 }
